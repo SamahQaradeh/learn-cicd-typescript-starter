b9ac5e700e5dcc0d472ab6fef8f24ee39bd94dbf
SamahQaradeh <samahqaradeh98@gmail.com>
2026-03-16 10:56:14 +0200

    correct sol

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,9 @@
-// eslint.config.js
 
-import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 
-export default defineConfig({
-  root: true,
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
+export default [
+  {
+    ignores: ["dist/**"],
   },
-  env: {
-    node: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  plugins: ["@typescript-eslint"],
-  rules: {
-    // optional custom rules
-    "no-unused-vars": "warn",
-    "semi": ["error", "always"]
-  }
-});
+  js.configs.recommended,
+];
