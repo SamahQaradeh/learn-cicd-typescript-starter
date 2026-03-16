c03d3967c9b6d1ad5926e1bbf003338bff6e0e9d
SamahQaradeh <samahqaradeh98@gmail.com>
2026-03-16 07:40:01 +0200

    Fix ESLint config and add TypeScript ESLint packages

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,25 @@
-
+// eslint.config.js
 import globals from "globals";
-import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  tseslint.configs.recommended,
+  // JavaScript files
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: ["js"],               // if you want js plugin
+    extends: ["plugin:js/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  // TypeScript files
+  {
+    files: ["**/*.{ts,mts,cts}"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    extends: ["plugin:@typescript-eslint/recommended"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
 ]);
